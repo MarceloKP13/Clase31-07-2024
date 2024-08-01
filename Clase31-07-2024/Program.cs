17b6feedc694c14e1fa1894cf4f3e3ea8765f7ef
Marcelo Torres <kevin.torres85@outlook.es>
2024-07-31 19:53:50 -0500

    clase 31/07/2024

---
+++ Clase31-07-2024/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase31_07_2024
+{
+    internal class Program
+    {
+        //crear una clases llamada matematica (suma resta y multi)
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingrese un número: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese otro número: ");
+            int b = int.Parse(Console.ReadLine());
+
+            matematicas mate = new matematicas();
+
+
+            int suma = mate.Sumar(a,b);
+            int resta = mate.Restar(a, b);
+            int multi = mate.Multiplicar(a, b);
+
+            Console.WriteLine($"La suma es: {suma}");
+            Console.WriteLine($"La resta es: {resta}");
+            Console.WriteLine($"El producto es: {multi}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Clase31-07-2024/matematicas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase31_07_2024
+{
+    public class matematicas
+    {
+        public int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Restar(int a, int b)
+        {
+            return a + b;
+        }
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+}
